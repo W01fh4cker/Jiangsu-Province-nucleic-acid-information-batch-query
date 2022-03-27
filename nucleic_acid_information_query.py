f00d2b5670f6f929d1fd5fbab0333d4d7cd64a56
W01fh4cker <sharecat2022@qq.com>
2022-03-27 20:53:55 +0800

    Update nucleic_acid_information_query.py

--- nucleic_acid_information_query.py
+++ nucleic_acid_information_query.py
@@ -4,7 +4,7 @@ print("""
 @version 1.0
 @2022/3/27
 ___          ______          ___        ________         ____          _______
-\  \        /  /\  \        /  /      /   ____  \       |   |         /  ____/
+\  \        /  __  \        /  /      /   ____  \       |   |         /  ____/
  \  \      /  /  \  \      /  /       |  |   |  |       |   |     __/   /___
   \  \    /  /    \  \    /  /        |  |   |  |       |   |    /___   ___/
    \  \  /  /      \  \  /  /         |  |   |  |       |   |       |  |
@@ -38,12 +38,17 @@ def nucleic_acid_information_query():
     style = xlwt.XFStyle()  # 创建样式对象
     style.pattern = pattern  # 将模式加入到样式对象
     # 设置单元格的宽度
-    output_worksheet.col(0).width = 1000 * 20
+    output_worksheet.col(1).width = 600 * 20
+    output_worksheet.col(2).width = 400 * 20
+    output_worksheet.col(3).width = 600 * 20
     # 写第一行的标题
-    output_worksheet.write(0, 0, '最近一次核酸报告时间', style)
-    output_worksheet.write(0, 1, '最近一次核酸检测结果(1表示阳性，2表示阴性)', style)
+    output_worksheet.write(0, 0, '姓名', style)
+    output_worksheet.write(0, 1, '最近一次核酸报告时间', style)
+    output_worksheet.write(0, 2, '最近一次核酸检测结果(1表示阳性，2表示阴性)', style)
+    output_worksheet.write(0, 3, '备注', style)
     for i in range(1, nrows):
         col_data1 = work_sheet.cell_value(i, 0)  # 获取第二列（姓名）的内容
+        name = col_data1
         col_data2 = '%s' % work_sheet.cell_value(i, 3)  # 获取第四列（身份证）的内容
         dict = {"username": col_data1, "userid": str(col_data2)}
         url = 'https://cydj.weiynet.cn/api/v3/getUserResult?info=' + str(json.dumps(dict,ensure_ascii=False)) + '&queryType=1&usercode=undefined&phone=undefined&source=h5&version=v3'
@@ -53,16 +58,22 @@ def nucleic_acid_information_query():
         resp = requests.post(url=url, headers=headers)
         res = json.loads((resp.content).decode('utf-8'))
         global report_time, report_result
+        output_worksheet.write(i,0,name)
         if (res["err"]==-3):
             print("[*]该人员信息有误")
+            status1 = "该人员信息有误"
+            output_worksheet.write(i, 3, status1)
         elif (res["err"]==0):
             if (len(res["list"]) == 0):
                 print("[*]"+ name +"该人员还没进行核酸检测！")
+                status2 = "该人员还没进行核酸检测！"
+                output_worksheet.write(i, 3, status2)
             else:
                 timechuo = res["list"][0]["testtime"]  # 获取时间戳
-                name = col_data1
                 if timechuo is None:
                     print("[*]"+ name + "核酸检测报告还未出来")
+                    status3 = "核酸检测报告还未出来"
+                    output_worksheet.write(i, 3, status3)
                 else:
                     if(len(res["list"])!=1):
                         j = 0
@@ -76,14 +87,15 @@ def nucleic_acid_information_query():
                     else:
                         report_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(timechuo)))
                         report_result = res["list"][0]["resultdata"]
+                        status5 = "无"
                         # 是否是阴性，1代表阳性，2代表阴性
                         if (report_result == "2"):
                             print("[*]"+ name + "为阴性，核酸检测报告出具时间为：" + str(report_time))
                         else:
                             print("[*]"+ name + "为阳性！！！！！！！！！，核酸检测报告出具时间为：" + str(report_time))
-                        # 写入数据
-                        output_worksheet.write(i, 0, report_time)
-                        output_worksheet.write(i, 1, report_result)
+                        output_worksheet.write(i, 1, report_time)
+                        output_worksheet.write(i, 2, report_result)
+                        output_worksheet.write(i, 3, status5)
                         time.sleep(0.5)
     output_workbook.save(make_time + '.xls')
     return again()
